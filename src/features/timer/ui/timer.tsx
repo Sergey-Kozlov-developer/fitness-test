8676cc44993f917020c3bb03023b15431d652d94
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2026-04-09 13:17:43 +0300

    feat: adding ui header

---
+++ src/features/timer/ui/timer.tsx
@@ -0,0 +1,12 @@
+export const Timer = () => {
+    return (
+        <div className="text-center py-2">
+            <p className="text-white text-2xl font-bold">
+                Успейте открыть пробную неделю
+            </p>
+            <p className="text-white text-4xl">
+                <span>+</span> 00 : 00 <span>+</span>
+            </p>
+        </div>
+    );
+};
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,9 +1,11 @@
-
+import { Timer } from "@features/timer";
 
 export const Header = () => {
-	return (
-		<div>
-
-		</div>
-	);
+    return (
+        <div className="ticky-nav bg-header">
+            <div className="container mx-auto">
+                <Timer />
+            </div>
+        </div>
+    );
 };
